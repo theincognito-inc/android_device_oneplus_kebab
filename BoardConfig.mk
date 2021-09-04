ce9e1f77e4392dd98ec31a573c7d3e46fe53897e
theincognito-inc <imags.bondoo7@gmail.com>
2021-09-04 16:19:33 +0000

    Removing build fingerprint overriding
    Might help with 9R flashing based on user's report (Experimental)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_BIND_SYSTEM := true
 TW_NO_EXFAT_FUSE := true
 TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+    "ro.build.product;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 TW_RECOVERY_ADDITIONAL_RELINK_BINARY_FILES += \
     $(TARGET_OUT_EXECUTABLES)/ashmemd \
     $(TARGET_OUT_EXECUTABLES)/strace
